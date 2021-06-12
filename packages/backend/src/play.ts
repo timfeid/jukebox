cef15e16ae3b9cf4f009308c888c2c2043be5a5c
Tim Feid <tfeid@exotix.us.com>
2021-06-12 18:36:12 -0400

    fix: maybe restart it, idk

--- packages/backend/src/play.ts
+++ packages/backend/src/play.ts
@@ -78,6 +78,7 @@ export class PlayerClass extends EventEmitter {
       ])
 
     stream.on('progress', p => {
+      console.log(p)
       this.updateProcess(p)
       console.log(p.timeElapsed)
       last = p.timeElapsed
