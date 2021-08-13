d25eb6ecc30933120365e587fab0c3015255cc9b
Tim Feid <tfeid@exotix.us.com>
2021-08-13 14:24:39 -0400

    fix: some more changes

--- packages/ytm-api/src/api/browse.ts
+++ packages/ytm-api/src/api/browse.ts
@@ -1,6 +1,5 @@
 import { createRequest } from "./create-request"
 import { parseBrowse } from "./parse/browse"
-import { SearchResult } from "./parse/search"
 
 
 export async function browse(browseId: string) {
@@ -13,7 +12,7 @@ export async function browse(browseId: string) {
         },
       },
     })
-    console.log(response.data.responseContext.serviceTrackingParams[0].params)
+    // console.log(response.data.responseContext.serviceTrackingParams[0].params)
     // console.log(response.data.header.musicDetailHeaderRenderer.title.runs)
     // console.log(response.data.header.musicDetailHeaderRenderer.subtitle.runs)
     return parseBrowse(response.data)
