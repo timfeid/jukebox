92ae691581f161810f8a097ab5e18f11702dcddd
Tim Feid <tfeid@exotix.us.com>
2021-08-13 13:33:31 -0400

    fix: add more

--- packages/tablet/components/Player.tsx
+++ packages/tablet/components/Player.tsx
@@ -1,9 +1,8 @@
-import Layout from './Layout'
-import SongImage from './SongImage'
 import { PlayerState, usePlayerContext } from '../context/player.context'
-import Styles from '../styles/Player.module.scss'
-import TopSongs from './TopSongs'
 import { request } from '../fetcher/graphql'
+import Styles from '../styles/Player.module.scss'
+import SongImage from './SongImage'
+import TopSongs from './TopSongs'
 
 function generateSongQueue(player: PlayerState) {
   if (player.queue.length === 0) {
@@ -66,18 +65,21 @@ export default function Home() {
         Continous play is {state.isPlayingContinuously ? 'on' : 'off'}
       </h1>
       <div className={Styles.grid}>
-        <div style={{gridTemplateColumns: 'repeat(4, minmax(0, 1fr))'}} className="grid justify-between w-full m-3">
+        <div style={{gridTemplateColumns: 'repeat(5, minmax(0, 1fr))'}} className="grid justify-between w-full m-3">
           {state.isPlayingContinuously && <div onClick={() => stopContinuouslyPlay()}>
             Turn off
           </div>}
           <div onClick={() => continuouslyPlay('VLRDTMAK5uy_kset8DisdE7LSD4TNjEVvrKRTmG7a56sY')}>
             Autoplay mix
           </div>
-          <div onClick={() => continuouslyPlay('VLPLY-ORNzeWA4-KT2scgfKiQrOmQ1ihDpHB')}>
-            Toons
+          <div onClick={() => continuouslyPlay('PLDoNWpWHi2nLqrJyJTf19nueOiEleQqVL')}>
+            Top 100 of 2000s
           </div>
           <div onClick={() => continuouslyPlay('PL0pMsxvRvrbxR4RwEE3Yc1ADk7lfJqmCx', true)}>
             Top 250 EDM of 2010s
+          </div>
+          <div onClick={() => continuouslyPlay('PLgrYDatnhwJKRKu5JWyh0WNjveSDHBIik', true)}>
+            Top 100 EDM
           </div>
         </div>
       </div>
